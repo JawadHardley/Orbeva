2ef3e0ebb078b87285bf1c0b7774289503196fcd
Jawad Charles <jawadcharls@gmail.com>
2025-08-30 20:01:25 +0300

    The Email Error 5

--- app/Http/Controllers/TransporterAuthController.php
+++ app/Http/Controllers/TransporterAuthController.php
@@ -379,7 +379,7 @@ class TransporterAuthController extends Controller
 
                 // Mail::to($mainVendor->email)->cc($ccEmails)->send(new NewAppMail($r, $mainVendor, $transporter));
                 // Mail::to($mainVendor->email)->cc($ccEmails)->queue(new NewAppMail($r, $mainVendor, $transporter));
-                Mail::to($mainVendor->email)->queue(new NewAppMail($r, $mainVendor, $transporter));
+                Mail::to($mainVendor->email)->send(new NewAppMail($r, $mainVendor, $transporter));
             }
 
             return redirect()
@@ -1224,7 +1224,7 @@ class TransporterAuthController extends Controller
                     $ccEmails = $vendors->skip(1)->pluck('email')->filter()->all(); // Remove nulls
 
                     // Mail::to($mainVendor->email)->cc($ccEmails)->queue(new NewAppMail($r, $mainVendor, $transporter));
-                    Mail::to($mainVendor->email)->queue(new NewAppMail($r, $mainVendor, $transporter));
+                    Mail::to($mainVendor->email)->send(new NewAppMail($r, $mainVendor, $transporter));
                 }
             } catch (\Exception $e) {
                 $errors[] = 'Row ' . ($rowIndex + 2) . ': Failed to insert. ' . $e->getMessage();
