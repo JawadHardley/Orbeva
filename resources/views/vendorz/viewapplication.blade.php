6f6127df3d46f12e7cdba9fc9d626379d450cdd5
Jawad Charles <jawadcharls@gmail.com>
2025-11-09 09:45:30 +0300

    CBM Fix for weights

--- resources/views/vendorz/viewapplication.blade.php
+++ resources/views/vendorz/viewapplication.blade.php
@@ -269,9 +269,9 @@
 
                         </div>
                         <!-- <h4 class="subheader mt-4">#Leave</h4>
-                                                                                                                                                                                                                                                                                                                                    <div class="list-group list-group-transparent">
-                                                                                                                                                                                                                                                                                                                                        <a href="#" class="list-group-item list-group-item-action">Give Feedback</a>
-                                                                                                                                                                                                                                                                                                                                    </div> -->
+                                                                                                                                                                                                                                                                                                                                        <div class="list-group list-group-transparent">
+                                                                                                                                                                                                                                                                                                                                            <a href="#" class="list-group-item list-group-item-action">Give Feedback</a>
+                                                                                                                                                                                                                                                                                                                                        </div> -->
                     </div>
                 </div>
                 <div class="col-12 col-md-10 d-flex flex-column tab-content">
@@ -457,6 +457,21 @@
                             <div class="col-12 mb-3 col-lg-4">
                                 <div class="form-label">Volume (Net Weight)T</div>
                                 <input type="text" name="volume" class="form-control" value="{{ $record->volume }}"
+                                    disabled>
+                            </div>
+
+                            <div class="col-12 mb-3 col-lg-4">
+                                <div class="form-label">TN</div>
+                                <input type="text" class="form-control" value="{{ $record->weight / 1000 }}"
+                                    disabled>
+                            </div>
+                            <div class="col-12 mb-3 col-lg-4">
+                                <div class="form-label">CBM</div>
+                                <input type="text" class="form-control" value="{{ $record->volume }}" disabled>
+                            </div>
+                            <div class="col-12 mb-3 col-lg-4">
+                                <div class="form-label">UP</div>
+                                <input type="text" class="form-control" value="{{ $record->weight / 1000 }}"
                                     disabled>
                             </div>
 
@@ -860,9 +875,9 @@
 
                         </div>
                         <!-- <h4 class="subheader mt-4">#Leave</h4>
-                                                                                                                                                                                                                                                                                                                                    <div class="list-group list-group-transparent">
-                                                                                                                                                                                                                                                                                                                                        <a href="#" class="list-group-item list-group-item-action">Give Feedback</a>
-                                                                                                                                                                                                                                                                                                                                    </div> -->
+                                                                                                                                                                                                                                                                                                                                        <div class="list-group list-group-transparent">
+                                                                                                                                                                                                                                                                                                                                            <a href="#" class="list-group-item list-group-item-action">Give Feedback</a>
+                                                                                                                                                                                                                                                                                                                                        </div> -->
                     </div>
                 </div>
                 <div class="col-12 col-md-10 d-flex flex-column tab-content">
@@ -1166,10 +1181,10 @@
                                                                 autocomplete="on" required />
 
                                                             <!-- <div class="col-12 col-md-6 mb-3">
-                                                                                                                                                                                                                                                                                                                                                    <label class="form-label">Customer Reference No</label>
-                                                                                                                                                                                                                                                                                                                                                    <input type="text" class="form-control" name="customer_ref"
-                                                                                                                                                                                                                                                                                                                                                        value="{{ $invoice->customer_ref ?? '' }}" autocomplete="on" required />
-                                                                                                                                                                                                                                                                                                                                                </div> -->
+                                                                                                                                                                                                                                                                                                                                                        <label class="form-label">Customer Reference No</label>
+                                                                                                                                                                                                                                                                                                                                                        <input type="text" class="form-control" name="customer_ref"
+                                                                                                                                                                                                                                                                                                                                                            value="{{ $invoice->customer_ref ?? '' }}" autocomplete="on" required />
+                                                                                                                                                                                                                                                                                                                                                    </div> -->
 
                                                             <!-- <label class="form-label">Customer Reference No</label> -->
                                                             <input type="hidden" class="form-control"
@@ -1419,8 +1434,8 @@
                             </div>
 
                             <!-- <div class="text-secondary">
-                                                                                                                                                                                                                                                                                                                                Do you want to proceed with the application ?
-                                                                                                                                                                                                                                                                                                                            </div> -->
+                                                                                                                                                                                                                                                                                                                                    Do you want to proceed with the application ?
+                                                                                                                                                                                                                                                                                                                                </div> -->
                         </div>
                         <div class="modal-footer">
                             <div class="w-100">
@@ -1476,8 +1491,8 @@
                                 <input type="file" class="form-control" name="file" required />
                             </div>
                             <!-- <div class="text-secondary">
-                                                                                                                                                                                                                                                                                                                                Do you want to proceed with the application ?
-                                                                                                                                                                                                                                                                                                                            </div> -->
+                                                                                                                                                                                                                                                                                                                                    Do you want to proceed with the application ?
+                                                                                                                                                                                                                                                                                                                                </div> -->
                         </div>
                         <div class="modal-footer">
                             <div class="w-100">
@@ -1673,11 +1688,11 @@
                     </form>
                 </div>
                 <!-- <div class="modal-footer">
-                                                                                                                                                                                                                                                                                                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                                                                                                                                                                                                                                                                                                        <button type="button" class="btn btn-primary">
-                                                                                                                                                                                                                                                                                                                            send <i class="fa fa-paper-plane ms-2"></i>
-                                                                                                                                                                                                                                                                                                                        </button>
-                                                                                                                                                                                                                                                                                                                    </div> -->
+                                                                                                                                                                                                                                                                                                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                                                                                                                                                                                                                                                                                                            <button type="button" class="btn btn-primary">
+                                                                                                                                                                                                                                                                                                                                send <i class="fa fa-paper-plane ms-2"></i>
+                                                                                                                                                                                                                                                                                                                            </button>
+                                                                                                                                                                                                                                                                                                                        </div> -->
             </div>
         </div>
     </div>
